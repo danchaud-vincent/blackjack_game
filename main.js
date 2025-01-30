b5a18a61a19f55e143c07a41fb97b09b409994e7
danchaud-vincent <vdanchaud@gmail.com>
2025-01-30 18:27:56 +0100

    updates files

---
+++ main.js
@@ -0,0 +1,131 @@
+let cardsList = [{name: "2_of_clubs", value: 2, source: 'sources/images/cards/2_of_clubs.png'},
+                 {name: "2_of_diamonds", value: 2, source: 'sources/images/cards/2_of_diamonds.png'},
+                 {name: "2_of_hearts", value: 2, source: 'sources/images/cards/2_of_hearts.png'},
+                 {name: "2_of_spades", value: 2, source: 'sources/images/cards/2_of_spades.png'},
+                 {name: "3_of_clubs", value: 3, source: 'sources/images/cards/3_of_clubs.png'},
+                 {name: "3_of_diamonds", value: 3, source: 'sources/images/cards/3_of_diamonds.png'},
+                 {name: "3_of_hearts", value: 3, source: 'sources/images/cards/3_of_hearts.png'},
+                 {name: "3_of_spades", value: 3, source: 'sources/images/cards/3_of_spades.png'},
+                 {name: "4_of_clubs", value: 4, source: 'sources/images/cards/4_of_clubs.png'},
+                 {name: "4_of_diamonds", value: 4, source: 'sources/images/cards/4_of_diamonds.png'},
+                 {name: "4_of_hearts", value: 4, source: 'sources/images/cards/4_of_hearts.png'},
+                 {name: "4_of_spades", value: 4, source: 'sources/images/cards/4_of_spades.png'},
+                 {name: "5_of_clubs", value: 5, source: 'sources/images/cards/5_of_clubs.png'},
+                 {name: "5_of_diamonds", value: 5, source: 'sources/images/cards/5_of_diamonds.png'},
+                 {name: "5_of_hearts", value: 5, source: 'sources/images/cards/5_of_hearts.png'},
+                 {name: "5_of_spades", value: 5, source: 'sources/images/cards/5_of_spades.png'},
+                 {name: "6_of_clubs", value: 6, source: 'sources/images/cards/6_of_clubs.png'},
+                 {name: "6_of_diamonds", value: 6, source: 'sources/images/cards/6_of_diamonds.png'},
+                 {name: "6_of_hearts", value: 6, source: 'sources/images/cards/6_of_hearts.png'},
+                 {name: "6_of_spades", value: 6, source: 'sources/images/cards/6_of_spades.png'},
+                 {name: "7_of_clubs", value: 7, source: 'sources/images/cards/7_of_clubs.png'},
+                 {name: "7_of_diamonds", value: 7, source: 'sources/images/cards/7_of_diamonds.png'},
+                 {name: "7_of_hearts", value: 7, source: 'sources/images/cards/7_of_hearts.png'},
+                 {name: "7_of_spades", value: 7, source: 'sources/images/cards/7_of_spades.png'},
+                 {name: "8_of_clubs", value: 8, source: 'sources/images/cards/8_of_clubs.png'},
+                 {name: "8_of_diamonds", value: 8, source: 'sources/images/cards/8_of_diamonds.png'},
+                 {name: "8_of_hearts", value: 8, source: 'sources/images/cards/8_of_hearts.png'},
+                 {name: "8_of_spades", value: 8, source: 'sources/images/cards/8_of_spades.png'},
+                 {name: "9_of_clubs", value: 9, source: 'sources/images/cards/9_of_clubs.png'},
+                 {name: "9_of_diamonds", value: 9, source: 'sources/images/cards/9_of_diamonds.png'},
+                 {name: "9_of_hearts", value: 9, source: 'sources/images/cards/9_of_hearts.png'},
+                 {name: "9_of_spades", value: 9, source: 'sources/images/cards/9_of_spades.png'},
+                 {name: "10_of_clubs", value: 10, source: 'sources/images/cards/10_of_clubs.png'},
+                 {name: "10_of_diamonds", value: 10, source: 'sources/images/cards/10_of_diamonds.png'},
+                 {name: "10_of_hearts", value: 10, source: 'sources/images/cards/10_of_hearts.png'},
+                 {name: "10_of_spades", value: 10, source: 'sources/images/cards/10_of_spades.png'},
+                 {name: "jack_of_clubs", value: 10, source: 'sources/images/cards/jack_of_clubs.png'},
+                 {name: "jack_of_diamonds", value: 10, source: 'sources/images/cards/jack_of_diamonds.png'},
+                 {name: "jack_of_hearts", value: 10, source: 'sources/images/cards/jack_of_hearts.png'},
+                 {name: "jack_of_spades", value: 10, source: 'sources/images/cards/jack_of_spades.png'},
+                 {name: "queen_of_clubs", value: 10, source: 'sources/images/cards/queen_of_clubs.png'},
+                 {name: "queen_of_diamonds", value: 10, source: 'sources/images/cards/queen_of_diamonds.png'},
+                 {name: "queen_of_hearts", value: 10, source: 'sources/images/cards/queen_of_hearts.png'},
+                 {name: "queen_of_spades", value: 10, source: 'sources/images/cards/queen_of_spades.png'},
+                 {name: "king_of_clubs", value: 10, source: 'sources/images/cards/king_of_clubs.png'},
+                 {name: "king_of_diamonds", value: 10, source: 'sources/images/cards/king_of_diamonds.png'},
+                 {name: "king_of_hearts", value: 10, source: 'sources/images/cards/king_of_hearts.png'},
+                 {name: "king_of_spades", value: 10, source: 'sources/images/cards/king_of_spades.png'},
+                 {name: "ace_of_clubs", value: 1, source: 'sources/images/cards/ace_of_clubs.png'},
+                 {name: "ace_of_diamonds", value: 1, source: 'sources/images/cards/ace_of_diamonds.png'},
+                 {name: "ace_of_hearts", value: 1, source: 'sources/images/cards/ace_of_hearts.png'},
+                 {name: "ace_of_spades", value: 1, source: 'sources/images/cards/ace_of_spades.png'}]
+
+let coinsList = [{name: "white_coin", value: 1, source:'sources/images/coins/white_coin.png'},
+                 {name: "red_coin", value: 5, source:'sources/images/coins/red_coin.png'},
+                 {name: "green_coin", value: 25, source:'sources/images/coins/green_coin.png'},
+                 {name: "blue_coin", value: 50, source:'sources/images/coins/blue_coin.png'},
+                 {name: "black_coin", value: 100, source:'sources/images/coins/black_coin.png'}]             
+
+
+let playerCardsEl = document.getElementById("player-cards-el")
+let giverCardsEl = document.getElementById("giver-cards-el")
+let playerScoreEl = document.getElementById("player-score-el")
+let giverScoreEl = document.getElementById("giver-score-el")
+
+let giver = {id:0, cards: [], sum: 0, hasBlackjack: false}
+let player = {id: 1, cards: [], sum: 0, hasBlackjack: false}
+
+
+function startGame(){
+    
+    for (let i=0; i<2; i++){
+        // giver draw
+        newCard(0)
+
+        //player draw
+        newCard(1)
+    }
+    
+    console.log(player)
+    console.log(giver)
+
+    // render the game
+    renderGame()
+}
+
+function newCard(id){
+    // get random card
+    let randomNumber = Math.floor(Math.random() * 52)
+    let randomCard = cardsList[randomNumber]
+
+    let elem = document.createElement("img")
+    elem.src = randomCard.source
+    
+    if (id === 0){
+        // add the image in the html
+        giverCardsEl.appendChild(elem)
+
+        // update giver object
+        giver["cards"].push(randomCard)
+        giver.sum += randomCard["value"]
+    }
+    else{
+        // add the image in the html
+        playerCardsEl.appendChild(elem)
+
+
+        // update player object
+        player["cards"].push(randomCard)
+        player.sum += randomCard["value"]
+    }
+
+    // render the game
+    renderGame()
+}
+
+function stay(){
+
+}
+
+
+function renderGame(){
+
+    // render the score
+    playerScoreEl.textContent = "Score: " + player.sum
+    giverScoreEl.textContent = "Score: " + giver.sum
+
+    // check if one of the player has a blackjack
+
+
+}
